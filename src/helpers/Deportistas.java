279b9ab7cb1f4b3af76e2a2121e6e3ce6e6c44aa
BRYAN-RODRIGUEZ-243 <101126221+BRYAN-RODRIGUEZ-243@users.noreply.github.com>
2022-03-10 23:39:38 -0600

    clase Objeto "Super clase" Personas Sub Clase Doctor Sub Clase Deportista

---
+++ src/helpers/Deportistas.java
@@ -0,0 +1,45 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author Breny
+ */
+public class Deportistas extends Doctor{
+private String Equipo;
+private String Deporte;
+
+
+//constructor 
+public Deportistas(){
+    
+}
+
+public String getnombre2(){
+    return "Roberto Carlos";
+}
+ public int getedad2(){
+        return 55;
+    }
+    
+ public void setEquipo(String Equipo){
+     this.Equipo = Equipo;
+ }
+ public String getEquipo(){
+     return Equipo;
+ }
+ public void setDeporte(String Deporte){
+    this.Deporte = Deporte;  
+ }
+ public String getDeporte(){
+     return Deporte;
+ }
+}
+
+
+
